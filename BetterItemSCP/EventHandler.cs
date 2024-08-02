f16af4216a87ee8a1dadff220d36b15defa015a2
damia <damia@ECLIPSE>
2024-08-02 16:38:42 +0200

    Aggiungere i file di progetto.

---
+++ BetterItemSCP/EventHandler.cs
@@ -0,0 +1,28 @@
+﻿using Exiled.Events.EventArgs.Player;
+
+namespace BetterItemSCP
+{
+    public class EventHandler
+    {
+        public void OnUsingItem(UsingItemCompletedEventArgs ev)
+        {
+            if (ev.Item.Type == ItemType.SCP500)
+            {
+                ev.Player.EnableEffect(Exiled.API.Enums.EffectType.Invigorated, 15f);
+                ev.Player.EnableEffect(Exiled.API.Enums.EffectType.MovementBoost, 15f);
+                ev.Player.AddAhp(amount: 75, sustain: 15, decay: 5);
+
+            }
+            if (ev.Item.Type == ItemType.SCP1853)
+            {
+                ev.Player.EnableEffect(Exiled.API.Enums.EffectType.DamageReduction, 15f);
+                ev.Player.EnableEffect(Exiled.API.Enums.EffectType.MovementBoost, 15f);
+                ev.Player.EnableEffect(Exiled.API.Enums.EffectType.Invisible, 7f);
+                ev.Player.AddAhp(amount: 75, sustain: 15, decay: 5);
+            }
+
+
+        } 
+
+}
+}
---
+++ BetterItemSCP/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Events;
+
+namespace BetterItemSCP
+{
+    public class BetterItemSCP : Plugin<Config>
+    {
+        private EventHandler _eventHandler;
+
+        public override string Name => "BetterItemSCP";
+        public override string Author => "Vacca";
+        public override string Prefix => "BetterItemSCP";
+        public override Version RequiredExiledVersion => new Version(8, 11, 0);
+
+        public override void OnEnabled()
+        {
+            _eventHandler = new EventHandler();
+            Exiled.Events.Handlers.Player.UsingItemCompleted += _eventHandler.OnUsingItem;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.UsingItemCompleted -= _eventHandler.OnUsingItem;
+            _eventHandler = null;
+            base.OnDisabled();
+        }
+    }
+}
