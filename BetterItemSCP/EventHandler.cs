178d5b0d9119e94f1c25d2c21b4d8783d1b3c619
Vacca <vacca1089@gmail.com>
2024-08-18 10:20:56 +0200

    Update EventHandler.cs

--- BetterItemSCP/EventHandler.cs
+++ BetterItemSCP/EventHandler.cs
@@ -1,28 +1,53 @@
-﻿using Exiled.Events.EventArgs.Player;
+using Exiled.API;
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using Exiled.Events.Commands.PluginManager;
+using Exiled.Events.EventArgs.Player;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BetterItemSCP
 {
     public class EventHandler
     {
+        private readonly BetterItemSCP plugin;
         public void OnUsingItem(UsingItemCompletedEventArgs ev)
         {
             if (ev.Item.Type == ItemType.SCP500)
-            {
-                ev.Player.EnableEffect(Exiled.API.Enums.EffectType.Invigorated, 15f);
-                ev.Player.EnableEffect(Exiled.API.Enums.EffectType.MovementBoost, 15f);
-                ev.Player.AddAhp(amount: 75, sustain: 15, decay: 5);
 
-            }
+                if (plugin.Config.Better500)
+                {
+                    List<Effect> Effects500 = plugin.Config.Effects500;
+
+                }
             if (ev.Item.Type == ItemType.SCP1853)
-            {
-                ev.Player.EnableEffect(Exiled.API.Enums.EffectType.DamageReduction, 15f);
-                ev.Player.EnableEffect(Exiled.API.Enums.EffectType.MovementBoost, 15f);
-                ev.Player.EnableEffect(Exiled.API.Enums.EffectType.Invisible, 7f);
-                ev.Player.AddAhp(amount: 75, sustain: 15, decay: 5);
-            }
+            
+                if (plugin.Config.Better1853)
+                {
+                    List<Effect> Effects1853 = plugin.Config.Effects1853;
+                }
 
+            if (ev.Item.Type == ItemType.SCP018)
 
-        } 
+                if (plugin.Config.Better018)
+                {
+                    List<Effect> Effects018 = plugin.Config.Effects018;
+                }
+            
+            if (ev.Item.Type == ItemType.SCP268)
 
-}
+                if (plugin.Config.Better268)
+                {
+                    List<Effect> Effects268 = plugin.Config.Effects268;
+                }
+
+            if (ev.Item.Type == ItemType.SCP207)
+
+                if (plugin.Config.Better207)
+                {
+                    List<Effect> Effects207 = plugin.Config.Effects207;
+                }
+        }
+
+    }
 }
